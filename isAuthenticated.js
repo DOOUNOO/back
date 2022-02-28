331c43d99de795b5f173d978278da51f36e1267d
HNVN-front <hoainamvalentinnguyen@gmail.com>
2022-02-28 19:07:14 +0100

    first commit

--- isAuthenticated.js
+++ isAuthenticated.js
@@ -10,7 +10,7 @@ const isAuthenticated = async (req, res, next) => {
       return res.status(401).json({ error: "Unauthorized" });
     } else {
       req.user = user;
-      // On crée une clé "user" dans req. La route dans laquelle le middleware est appelé     pourra avoir accès à req.user
+      // On crée une clé "user" dans req. La route dans laquelle le middleware est appelé pourra avoir accès à req.user
       return next();
     }
   } else {
